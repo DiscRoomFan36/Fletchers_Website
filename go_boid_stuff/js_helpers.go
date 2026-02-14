59c88f11f6102bd7d370a511790ce7c4677c1f78
DiscRoomFan36 <fletcherhogarth@gmail.com>
2026-02-14 18:52:57 +1100

    strict type checking in reflection

--- go_boid_stuff/js_helpers.go
+++ go_boid_stuff/js_helpers.go
@@ -3,216 +3,15 @@ package main
 import (
 	"fmt"
 	"reflect"
-	"strings"
+	"slices"
 	"syscall/js"
 	"unsafe"
 )
 
-func js_value_to_json(value js.Value) string {
-	b := strings.Builder{}
+// will make 'parse_js_value_to_type()' panic if the js object has any extra fields
+const STRICT_OBJECT_PARSING = true
 
-	switch value.Type() {
-	case js.TypeBoolean: {
-		as_bool := value.Bool()
-		if as_bool { b.WriteString("True")
-		} else     { b.WriteString("False") }
-	}
-
-	case js.TypeObject: {
-		b.WriteString("json: ")
-		b.WriteString(value.String())
-	}
-
-	default: {
-		panic(fmt.Errorf("js_value_to_json: Unknown Type %s", value.Type().String()))
-	}
-	}
-
-	return b.String()
-}
-
-func set_unsafe_pointer_to_T[T any](unsafe_pointer unsafe.Pointer, value T) {
-	as_T_ptr := (*T)(unsafe_pointer)
-	*as_T_ptr = value
-}
-
-// in my house, the error goes first. harder to ignore.
-func js_value_to_struct[T any](value js.Value) (T, error) {
-
-	var result T
-	result_base_pointer := unsafe.Pointer(&result)
-
-	type Item struct {
-		type_to_parse    reflect.Type
-		js_value         js.Value
-		base_pointer     unsafe.Pointer
-	}
-
-	stack := []Item{}
-	Append(&stack, Item{
-		type_to_parse : reflect.TypeFor[T](),
-		js_value      : value,
-		base_pointer  : result_base_pointer,
-	})
-
-	// for quick error checking
-	type_kind_to_js_type := func(kind reflect.Kind) js.Type {
-		switch kind {
-		case reflect.Bool:    { return js.TypeBoolean }
-
-		case reflect.Int:     { return js.TypeNumber  }
-		case reflect.Float64: { return js.TypeNumber  }
-		case reflect.Float32: { return js.TypeNumber  }
-
-		case reflect.Struct:  { return js.TypeObject  }
-		case reflect.Slice:   { return js.TypeObject  }
-
-		default: { panic(fmt.Errorf("unknown kind of type %v", kind)) }
-		}
-	}
-
-
-	for len(stack) > 0 {
-		item := Pop(&stack)
-
-		if item.js_value.IsNull() || item.js_value.IsUndefined() {
-			return result, fmt.Errorf("When trying to parse '%v' type, js.value was null or undefined", item.type_to_parse)
-		}
-
-		{ // quickly check the type of js.value
-			current_type_kind := item.type_to_parse.Kind()
-			current_js_type   := item.js_value.Type()
-
-			correct_js_type := type_kind_to_js_type(current_type_kind)
-
-			if correct_js_type != current_js_type {
-				return result, fmt.Errorf("element of type '%s' expects a js.value of type '%s', got '%s' instead", current_type_kind.String(), correct_js_type.String(), current_js_type.String())
-			}
-		}
-
-		switch item.type_to_parse.Kind() {
-		case reflect.Bool: {
-			set_unsafe_pointer_to_T(item.base_pointer, item.js_value.Bool())
-		}
-
-		case reflect.Int: {
-			set_unsafe_pointer_to_T(item.base_pointer, int(item.js_value.Int()))
-		}
-
-		case reflect.Float64: {
-			set_unsafe_pointer_to_T(item.base_pointer, item.js_value.Float())
-		}
-
-		case reflect.Float32: {
-			set_unsafe_pointer_to_T(item.base_pointer, float32(item.js_value.Float()))
-		}
-
-		case reflect.Struct: {
-			// heres where it all falls apart...
-
-			// check if the type is js.value, this just means the user
-			// has no idea how to type this thing correctly, so we are
-			// just going to pass the whole thing to them
-			//
-			// also golang `rules for thee but not for me`
-			if item.type_to_parse == reflect.TypeFor[js.Value]() {
-				set_unsafe_pointer_to_T(item.base_pointer, item.js_value)
-			} else {
-
-				// loop over all fields, put into stack.
-				for i := range item.type_to_parse.NumField() {
-					new_field := item.type_to_parse.Field(i)
-					new_value := item.js_value.Get(new_field.Name)
-
-					if new_value.IsNull() || new_value.IsUndefined() {
-						return result, fmt.Errorf("Cannot find field %s in js.value %s", new_field.Name, js_value_to_json(item.js_value))
-					}
-
-					Append(&stack, Item{
-						type_to_parse : new_field.Type,
-						js_value      : new_value,
-						base_pointer  : unsafe.Add(item.base_pointer, new_field.Offset),
-					})
-				}
-			}
-		}
-
-		case reflect.Slice: {
-			slice_element_type := item.type_to_parse.Elem()
-
-			// its funny, we cannot tell the difference between an array and a
-			// "map", lets hope you got the types right!
-			js_array_len := item.js_value.Length()
-
-			// should i give this extra capacity? nah. it's confusing enough as is.
-			generic_slice := reflect.MakeSlice(item.type_to_parse, js_array_len, js_array_len)
-
-
-			// unsafe pointer actually points to the first element in the slice,
-			// witch is not what we want.
-			//
-			// we want to set the base pointer to a slice with data = that pointer,
-			// and len and cap be the right thing.
-			first_element_in_generic_slice := generic_slice.UnsafePointer()
-
-			// we need to set the slice header thats in the struct. unfortunately
-			// the capacity is not saved, also i think the memory might leak?
-			//
-			// @Garbage-collector this real array probably isn correctly registered.
-			// lets hope you copy the array out or something,
-			fake_slice_but_real_header := unsafe.Slice((*byte)(first_element_in_generic_slice), js_array_len)
-			set_unsafe_pointer_to_T(item.base_pointer, fake_slice_but_real_header)
-
-			// now we just need to load the slice.
-			//
-			// lets hope the array isn't to long,
-			for slice_index := range js_array_len {
-
-				slice_base_pointer := generic_slice.UnsafePointer()
-
-				pointer_to_element_in_slice := unsafe.Add(slice_base_pointer, slice_index * int(slice_element_type.Size()))
-
-				Append(&stack, Item{
-					type_to_parse : slice_element_type,
-					// every element in array will get type check eventually.
-					js_value      : item.js_value.Index(slice_index),
-					// look! they match! very funny
-					base_pointer  : pointer_to_element_in_slice,
-				})
-			}
-		}
-
-		default: {
-			return result, fmt.Errorf("type '%s' is unknown, and is currently not settable", item.type_to_parse.String())
-		}
-		}
-	}
-
-	return result, nil
-}
-
-
-	// for quick error checking
-func reflect_kind_to_js_type(kind reflect.Kind) js.Type {
-	switch kind {
-	case reflect.Bool:    { return js.TypeBoolean }
-
-	case reflect.Int:     { return js.TypeNumber  }
-	case reflect.Float64: { return js.TypeNumber  }
-	case reflect.Float32: { return js.TypeNumber  }
-
-	case reflect.Struct:  { return js.TypeObject  }
-	case reflect.Slice:   { return js.TypeObject  }
-	case reflect.Map:     { return js.TypeObject  }
-
-	case reflect.String:  { return js.TypeString  }
-
-	default: { panic(fmt.Errorf("unknown kind of type %v", kind)) }
-	}
-}
-
-
-
+// NOTE: I would make a test for this function, but syscall/js is not having a good time.
 func parse_js_value_to_type[T any](js_value js.Value) (T, error) {
 	var result T
 
@@ -226,6 +25,19 @@ func parse_js_value_to_type[T any](js_value js.Value) (T, error) {
 func parse_js_value_to_type_helper(js_value js.Value, reflected_type reflect.Value, name string) error {
 	if !reflected_type.CanSet() { panic("cannot set reflected type for some reason?") }
 
+
+	{ // quickly check the type of js.value
+		current_type_kind := reflected_type.Kind()
+		current_js_type   := js_value.Type()
+
+		correct_js_type   := reflect_kind_to_js_type(current_type_kind)
+
+		if correct_js_type != current_js_type {
+			return fmt.Errorf("element of type '%s' expects a js.value of type '%s', got '%s' instead", current_type_kind.String(), correct_js_type.String(), current_js_type.String())
+		}
+	}
+
+
 	switch reflected_type.Kind() {
 	case reflect.Bool    : { reflected_type.SetBool (      js_value.Bool() ) }
 	case reflect.Int     : { reflected_type.SetInt  (int64(js_value.Int()) ) }
@@ -238,10 +50,77 @@ func parse_js_value_to_type_helper(js_value js.Value, reflected_type reflect.Val
 		// just going to pass the whole thing to them
 		if reflected_type.Type() == reflect.TypeFor[js.Value]() {
 
-			reflected_js_field := reflect.ValueOf(js_value)
-			reflected_type.Set(reflected_js_field)
+			reflected_type.Set(reflect.ValueOf(js_value))
 
 		} else {
+
+			// this wouldn't work anyway, but this is a better message
+			if js_object_is_array(js_value) {
+				return fmt.Errorf("expected to parse a struct, but js_value was an array, not an object.")
+			}
+
+			// if this is enabled, we make sure all the
+			// fields are in the object, no more, no less
+			if STRICT_OBJECT_PARSING {
+				js_object_keys := js_get_keys_for_object(js_value)
+
+				struct_keys := []string{}
+				for i := range reflected_type.NumField() {
+					Append(&struct_keys, reflected_type.Type().Field(i).Name)
+				}
+
+				// sort them so its easier to check what keys are there, and what are missing.
+				slices.Sort(js_object_keys)
+				slices.Sort(struct_keys)
+
+				// keys the js object has that the struct doesn't
+				extra_keys := []string{}
+				// keys the struct has but the js object doesn't
+				missing_keys := []string{}
+				// // both share these keys
+				// correct_keys := []string{}
+
+				{
+					j := 0
+					for _, struct_key := range struct_keys {
+						for js_object_keys[j] < struct_key {
+							Append(&extra_keys, js_object_keys[j])
+							j += 1
+						}
+
+						if struct_key == js_object_keys[j] {
+							j += 1
+							// Append(&correct_keys, struct_key)
+							continue
+						} else {
+							Append(&missing_keys, struct_key)
+						}
+					}
+
+					// the other keys that we missed
+					Append(&extra_keys, js_object_keys[j:]...)
+				}
+
+				// oh no!
+				is_missing_keys := len(missing_keys) > 0
+				has_extra_keys  := len(extra_keys)   > 0
+				if is_missing_keys || has_extra_keys {
+					if is_missing_keys && !has_extra_keys {
+						// only missing keys
+						return fmt.Errorf("js object is missing keys, missing these key(s): %v", missing_keys)
+
+					} else if has_extra_keys && !is_missing_keys {
+						// js object has extra keys
+						return fmt.Errorf("js object has extra keys, this is not allowed by STRICT_OBJECT_PARSING, extra key(s): %v", extra_keys)
+
+					} else {
+						// they have different sets of keys altogether
+						return fmt.Errorf("js object is both: missing some keys (%v), and has extra fields than expected (%v), did you get your structure definitions right?", missing_keys, extra_keys)
+					}
+				}
+
+				// everything is fine.
+			}
 
 			// lets parse all the fields!
 			for i := range reflected_type.NumField() {
@@ -249,17 +128,6 @@ func parse_js_value_to_type_helper(js_value js.Value, reflected_type reflect.Val
 				struct_field := reflected_type.Type().Field(i)
 
 				js_field     := js_value.Get(struct_field.Name)
-
-				{ // quickly check the type of js.value
-					current_type_kind := field_value.Kind()
-					current_js_type   := js_field.Type()
-
-					correct_js_type   := reflect_kind_to_js_type(current_type_kind)
-
-					if correct_js_type != current_js_type {
-						return fmt.Errorf("element of type '%s' expects a js.value of type '%s', got '%s' instead", current_type_kind.String(), correct_js_type.String(), current_js_type.String())
-					}
-				}
 
 				// fell down a massive rabbit hole, thank you random medium article.
 				//
@@ -346,7 +214,6 @@ func parse_js_value_to_type_helper(js_value js.Value, reflected_type reflect.Val
 }
 
 
-// NOTE: I would make a test here, but syscall/js is not having a good time.
 
 
 func js_object_is_array(js_value js.Value) bool {
@@ -355,10 +222,10 @@ func js_object_is_array(js_value js.Value) bool {
 	return is_a_js_array
 }
 
-// TODO error checking in here
 func js_get_keys_for_object(js_value js.Value) []string {
-
 	// this thing has a lot of good functions in it.
+	//
+	// also this thing is known to exist.
 	js_object_constructor := js.Global().Get("Object")
 
 	keys_as_js_array := js_object_constructor.Call("keys", js_value)
@@ -370,4 +237,28 @@ func js_get_keys_for_object(js_value js.Value) []string {
 
 	return result
 }
+
+
+
+// internal function
+//
+// for quick error checking
+func reflect_kind_to_js_type(kind reflect.Kind) js.Type {
+	switch kind {
+	case reflect.Bool:    { return js.TypeBoolean }
+
+	case reflect.Int:     { return js.TypeNumber  }
+	case reflect.Float64: { return js.TypeNumber  }
+	case reflect.Float32: { return js.TypeNumber  }
+
+	case reflect.Struct:  { return js.TypeObject  }
+	case reflect.Slice:   { return js.TypeObject  }
+	case reflect.Map:     { return js.TypeObject  }
+
+	case reflect.String:  { return js.TypeString  }
+
+	default: { panic(fmt.Errorf("unknown kind of type %v", kind)) }
+	}
+}
+
 
