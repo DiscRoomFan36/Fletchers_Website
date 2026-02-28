0b9bd190106f9a68041dfd690978ce62eb3bab35
DiscRoomFan36 <fletcherhogarth@gmail.com>
2026-02-28 14:28:10 +1100

    now that categories are a thing, removed the debug at the start of the name

--- go_boid_stuff/drawing.go
+++ go_boid_stuff/drawing.go
@@ -15,11 +15,11 @@ const SCALE_FACTOR = 1 / BOID_SCALE;
 func Draw_Everything(img *Image, boid_sim *Boid_simulation, dt float64, input User_Input) {
     Draw_Cool_Background(img, boid_sim, dt, input);
 
-    if boid_sim.properties.Debug_Draw_Spacial_Array {
+    if boid_sim.properties.Draw_Spacial_Array {
         draw_spacial_array_into_image(img, boid_sim.Spacial_array, SCALE_FACTOR);
     }
 
-    if boid_sim.properties.Debug_Draw_Boundary && boid_sim.properties.Toggle_Bounding {
+    if boid_sim.properties.Draw_Boundary && boid_sim.properties.Toggle_Bounding {
         boid_boundary_color := rgb(240, 240, 240);
 
         margin := int(boid_sim.properties.Margin * SCALE_FACTOR);
@@ -35,7 +35,7 @@ func Draw_Everything(img *Image, boid_sim *Boid_simulation, dt float64, input Us
         }
     }
 
-    if boid_sim.properties.Debug_Draw_Visual_Ranges {
+    if boid_sim.properties.Draw_Visual_Ranges {
         // Draw visual radius.
         visual_radius_color := HSL_to_RGB(50, 0.7, 0.9);
         for _, b := range boid_sim.Boids {
@@ -55,7 +55,7 @@ func Draw_Everything(img *Image, boid_sim *Boid_simulation, dt float64, input Us
         }
     }
 
-    if boid_sim.properties.Debug_Draw_Rectangles {
+    if boid_sim.properties.Draw_Rectangles {
         for _, wall := range boid_sim.Rectangles {
             x, y, w, h := wall.Splat();
             boundary_points := [4]Vec2[Boid_Float]{
@@ -146,7 +146,7 @@ func Draw_Everything(img *Image, boid_sim *Boid_simulation, dt float64, input Us
         Draw_Triangle(img, boid_shape[0], boid_shape[1], boid_shape[2], boid_color);
         Draw_Triangle(img, boid_shape[0], boid_shape[1], boid_shape[3], boid_color);
 
-        if boid_sim.properties.Debug_Draw_Heading {
+        if boid_sim.properties.Draw_Heading {
             // Draw heading line
             where_boid_will_be := Add(b.Position, b.Velocity);
             Draw_Line(img, b.Position, where_boid_will_be, rgba(43, 231, 26, 1));
@@ -234,7 +234,7 @@ func Draw_Everything(img *Image, boid_sim *Boid_simulation, dt float64, input Us
         );
     }
 
-    if boid_sim.properties.Debug_Draw_Mouse_Position {
+    if boid_sim.properties.Draw_Mouse_Position {
     	center   := Mult(input.Mouse_Position, SCALE_FACTOR);
 
     	const LEN = 10
